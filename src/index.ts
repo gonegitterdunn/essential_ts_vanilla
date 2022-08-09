1f5e43313420b4c45706d4f5395ec1546aa871c7
Gitter Dunn <gitterdunn@protonmail.com>
2022-08-09 10:23:32 +0200

    add local data source, add bootstrap to webpack, update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-console.log('Web App');
-console.log("hihi");
+import { LocalDataSource } from './data/localDataSource';
+import { DomDisplay } from './domDisplay';
+
+let ds = new LocalDataSource();
+
+async function displayData(): Promise<HTMLElement> {
+  let display = new DomDisplay();
+  display.props = {
+    products: await ds.getProducts('name'),
+    order: ds.order,
+  };
+  return display.getContent();
+}
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    displayData().then((elem) => {
+      let rootElement = document.getElementById('app');
+      rootElement.innerHTML = '';
+      rootElement.appendChild(elem);
+    });
+  }
+};
